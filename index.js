5e3954d22c87c1f18fae801f6c69c335699303ca
byronsk <vyskoutaris@hotmail.com>
2019-04-08 11:47:44 +0100

    Done

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+   static capitalize(string) {
+     return string[0].toUpperCase() + string.slice(1)
+}
+
+  static sanitize(str) {
+    str = str.replace(/[^a-z0-9áéíóúñü_-\s\.,]/gim,"");
+   return str.trim();
+  }
+
+  static titleize(string) {
+  var words = string.match(/\b\w+\b/g),
+      preps = ["an", "but", "of", "and", "for", "by", "a", "the", "from", "at"];
+      return words.map(function(e,i) {
+      return preps.indexOf(e) == -1 || i === 0 ? e[0].toUpperCase()+e.slice(1) : e;
+      }).join(" ");
+}
+
 }
