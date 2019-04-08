a5313a1ce36efdf958495719925b28037098e5b5
byronsk <vyskoutaris@hotmail.com>
2019-04-08 11:48:23 +0100

    Done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class Formatter {
   }
 
   static titleize(string) {
-  var words = string.match(/\b\w+\b/g),
+  let words = string.match(/\b\w+\b/g),
       preps = ["an", "but", "of", "and", "for", "by", "a", "the", "from", "at"];
       return words.map(function(e,i) {
       return preps.indexOf(e) == -1 || i === 0 ? e[0].toUpperCase()+e.slice(1) : e;
